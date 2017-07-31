a48f857b5784e64772688587484f77902787cbe3
shmin <shmin.dev@gmail.com>
2017-07-31 14:54:01 +0900

    [Tizen] Add ControlGallery.Tizen project

---
+++ Xamarin.Forms.ControlGallery.Tizen/StringProvider.cs
@@ -0,0 +1,16 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.ControlGallery.Tizen;
+using Xamarin.Forms.Controls;
+
+[assembly: Dependency(typeof(StringProvider))]
+
+namespace Xamarin.Forms.ControlGallery.Tizen
+{
+	public class StringProvider : IStringProvider
+	{
+		public string CoreGalleryTitle
+		{
+			get { return "Tizen Core Gallery"; }
+		}
+	}
+}

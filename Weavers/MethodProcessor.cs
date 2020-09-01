0172e10159564a901ba2d506d41beb392ce074b1
민성현/Common Platform Lab(SR)/Staff Engineer/삼성전자 <shyun.min@samsung.com>
2020-09-01 16:15:00 +0900

    [Tizen] Add FodyWeaver to analyze test coverage (#469)

---
+++ Weavers/MethodProcessor.cs
@@ -0,0 +1,22 @@
+﻿using Mono.Cecil.Cil;
+using System.Collections.Generic;
+
+public static class IlProcessorExtensions
+{
+	public static void InsertBefore(this ILProcessor processor, Instruction target, IEnumerable<Instruction> instructions)
+	{
+		foreach (var instruction in instructions)
+		{
+			processor.InsertBefore(target, instruction);
+		}
+	}
+
+	public static void InsertAfter(this ILProcessor processor, Instruction target, IEnumerable<Instruction> instructions)
+	{
+		foreach (var instruction in instructions)
+		{
+			processor.InsertAfter(target, instruction);
+			target = instruction;
+		}
+	}
+}
